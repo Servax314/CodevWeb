2697636aad851b8b7e7a8e34fabef926b23ce656
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-04-18 11:54:49 +0200

    big changes

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
-// modules =================================================
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const morgan = require('morgan');
-const cors = require('cors');
-const rfs = require('rotating-file-stream')
-const log4js = require('log4js');
-var MongoStore = require('connect-mongo')(session);
-const Queue = require('bull');
-const { setQueues, UI } = require('bull-board');
+const express = require('express')
+const app = express()
 
-const https = require('https');
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+app.set('view-engine', 'jade')
 
-const app = new express();
+app.get('/', (req,res) => {
+  res.render('login.jade')
+})
 
-let handleRequest = (request, response) => {
-    app.get('/', function(req,resp) {
-        reponse.sendFile('./src/accueil.html')
-    });
-};
+app.get('/login', (req,res) => {
+  res.render('login.jade')
+})
 
-http.createServer(handleRequest).listen(3000);
+app.get('/accueil', (req,res) => {
+  res.render('accueil.jade')
+})
+
+app.listen(3000)
