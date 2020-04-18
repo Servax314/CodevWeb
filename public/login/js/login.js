444317f09a18eafe37692ab7577238f80c476d6f
pmichel610 <61125021+pmichel610@users.noreply.github.com>
2020-04-18 18:36:59 +0200

    Update login.js

--- public/login/js/login.js
+++ public/login/js/login.js
@@ -1,16 +1,16 @@
 function register(){
     
     var obj = document.getElementById("loginForm");
-    var x = '<div id="registerForm" class="registerForm">'
+    var x = '<div id="registerForm" class="form">'
                +'<form action="/login" method="POST">'
                +'<label>Mail address :</label>'
-               +'<input id="mailAddress" name="mailAddress" type="text" autocapitalize="off" autocorrect="off"/></br>'
+               +'<input class="field" id="mailAddress" name="mailAddress" type="text" autocapitalize="off" autocorrect="off"/></br>'
                + '<label>Login :</label>'
-               +'<input id="Username" name="Username" type="text" autocapitalize="off" autocorrect="off"/></br>'
+               +'<input class="field" id="Username" name="Username" type="text" autocapitalize="off" autocorrect="off"/></br>'
                + '<label> Password : </label>'
-               + '<input id="Password" name="Password" type="Password" autocapitalize="off" autocorrect="off"/></br>'
+               + '<input class="field" id="Password" name="Password" type="Password" autocapitalize="off" autocorrect="off"/></br>'
                + '<label>Repeat Password : </label>'
-               + '<input id="Password" name="Password" type="Password" autocapitalize="off" autocorrect="off"/></br>'
+               + '<input class="field" id="Password" name="Password" type="Password" autocapitalize="off" autocorrect="off"/></br>'
                +'<input id="registerButton" name="registerButton" type="submit" value="Register">'
                + '</form>'
                +'</div>'
