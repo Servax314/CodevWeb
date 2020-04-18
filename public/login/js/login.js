67a70fefd23081951cf4f85e4ff2035c70f43aa0
pmichel610 <61125021+pmichel610@users.noreply.github.com>
2020-04-18 15:50:24 +0200

    Update login.js

--- public/login/js/login.js
+++ public/login/js/login.js
@@ -2,18 +2,18 @@ function register(){
     
     var obj = document.getElementById("loginForm");
     var x = '<div id="registerForm" class="registerForm">'
-            +'<form action="/register" method="POST">'
-            +'<label>Mail address :</label>'
-             +'<input id="mailAddress" name="mailAddress" type="text" autocapitalize="off" autocorrect="off"/></br>'
+               +'<form action="/login" method="POST">'
+               +'<label>Mail address :</label>'
+               +'<input id="mailAddress" name="mailAddress" type="text" autocapitalize="off" autocorrect="off"/></br>'
                + '<label>Login :</label>'
-                +'<input id="Username" name="Username" type="text" autocapitalize="off" autocorrect="off"/></br>'
+               +'<input id="Username" name="Username" type="text" autocapitalize="off" autocorrect="off"/></br>'
                + '<label> Password : </label>'
                + '<input id="Password" name="Password" type="Password" autocapitalize="off" autocorrect="off"/></br>'
                + '<label> Password : </label>'
                + '<input id="Password" name="Password" type="Password" autocapitalize="off" autocorrect="off"/></br>'
-                +'<input id="registerButton" name="registerButton" type="submit" value="Register">'
-           + '</form>'
-        +'</div>'
+               +'<input id="registerButton" name="registerButton" type="submit" value="Register">'
+               + '</form>'
+               +'</div>'
     obj.innerHTML = x;
 
 }
