9c0b51678fbf033710d1370cc963a3c73b1719b3
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-04-19 15:14:39 +0200

    js on login frontend

---
+++ public/login/js/switchLogReg.js
@@ -0,0 +1,39 @@
+let i = true;
+
+
+let registerHtml = '<div id="registerForm" class="form">'
+           +'<form action="/login" method="POST">'
+           +'<label>Mail address :</label>'
+           +'<input class="field" id="mailAddress" name="mailAddress" type="text" autocapitalize="off" autocorrect="off"/></br>'
+           +'<label>Login :</label>'
+           +'<input class="field" id="username" name="username" type="text" autocapitalize="off" autocorrect="off"/></br>'
+           +'<label> Password : </label>'
+           +'<input class="field" id="password" name="password" type="password" autocapitalize="off" autocorrect="off"/></br>'
+           +'<label>Repeat Password : </label>'
+           +'<input class="field" id="password" name="password" type="password" autocapitalize="off" autocorrect="off"/></br>'
+           +'<input id="registerButton" name="registerButton" type="submit" value="Register">'
+           +'</form>'
+           +'</div>';
+
+  let loginHtml = '<div id="loginForm" class="form">'
+            +  '<form action="/login" method="POST">'
+             +     '<label  >Username :</label>'
+              +    '<input class="field" id="username" name="username" type="text" autocapitalize="off" autocorrect="off"/></br></br>'
+           +   '<label > Password : </label>'
+             +   '<input class="field" id="password" name="password" type="password" autocapitalize="off" autocorrect="off"/></br>'
+             +   '<input id="loginButton" name="loginButton" type="submit" value="Log In">'
+             + '</form>'
+          +'</div>';
+
+function switchHtml(){
+  if (i){
+    i = false;
+    var obj = document.getElementById("loginForm");
+    obj.innerHTML = registerHtml;
+
+  }else{
+    var obj = document.getElementById("registerForm");
+    obj.innerHTML = loginHtml;
+    i=true;
+  }
+};
