9fbc2fa52eaa6c537a27617b6303b8660bba847d
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-04-19 10:56:19 +0200

    Update login.js

--- public/login/js/login.js
+++ public/login/js/login.js
@@ -1,20 +1,27 @@
-function register(){
-    
+let i = 0
+let registerHtml = '<div id="registerForm" class="form">'
+           +'<form action="/login" method="POST">'
+           +'<label>Mail address :</label>'
+           +'<input class="field" id="mailAddress" name="mailAddress" type="text" autocapitalize="off" autocorrect="off"/></br>'
+           +'<label>Login :</label>'
+           +'<input class="field" id="Username" name="Username" type="text" autocapitalize="off" autocorrect="off"/></br>'
+           +'<label> Password : </label>'
+           +'<input class="field" id="Password" name="Password" type="Password" autocapitalize="off" autocorrect="off"/></br>'
+           +'<label>Repeat Password : </label>'
+           +'<input class="field" id="Password" name="Password" type="Password" autocapitalize="off" autocorrect="off"/></br>'
+           +'<input id="registerButton" name="registerButton" type="submit" value="Register">'
+           +'</form>'
+           +'</div>'
+
+  let loginHtml = ''
+
+function updateHtml(){
+  i +=1;
+  if {
     var obj = document.getElementById("loginForm");
-    var x = '<div id="registerForm" class="form">'
-               +'<form action="/login" method="POST">'
-               +'<label>Mail address :</label>'
-               +'<input class="field" id="mailAddress" name="mailAddress" type="text" autocapitalize="off" autocorrect="off"/></br>'
-               + '<label>Login :</label>'
-               +'<input class="field" id="Username" name="Username" type="text" autocapitalize="off" autocorrect="off"/></br>'
-               + '<label> Password : </label>'
-               + '<input class="field" id="Password" name="Password" type="Password" autocapitalize="off" autocorrect="off"/></br>'
-               + '<label>Repeat Password : </label>'
-               + '<input class="field" id="Password" name="Password" type="Password" autocapitalize="off" autocorrect="off"/></br>'
-               +'<input id="registerButton" name="registerButton" type="submit" value="Register">'
-               + '</form>'
-               +'</div>'
-    obj.innerHTML = x;
+    obj.innerHTML = registerHtml;
+  }else{
+    var obj = document.getElementById("registerForm");
+    obj.innerHTML = loginHtml;
+  }
 }
-
-
