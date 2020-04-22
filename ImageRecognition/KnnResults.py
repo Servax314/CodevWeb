e2374b548a7be80b535a88a593985030315fbf9b
HugoBcz <hugo.bouchez@orange.fr>
2020-04-22 16:03:04 +0200

    Add new model, actualize score

--- ImageRecognition/KnnResults.py
+++ ImageRecognition/KnnResults.py
@@ -15,18 +15,21 @@ print('Open testing features')
 Target = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 #Import model
-knn = load('knn.joblib')
+model = load('model.joblib')
+
+X_test = np.array(X_test)
+Y_test = np.array(Y_test)
 
 #score
-score = knn.score(X_test,Y_test)
+score = model.score(X_test,Y_test)
 print(score)
 
 np.set_printoptions(precision=2)
 
 # Plot non-normalized confusion matrix
-titles_options = [("Confusion matrix, without normalization", None)]
+""" titles_options = [("Confusion matrix, without normalization", None)]
 for title, normalize in titles_options:
-    disp = plot_confusion_matrix(knn, X_test, Y_test,
+    disp = plot_confusion_matrix(model, X_test, Y_test,
                                  display_labels=Target,
                                  cmap=plt.cm.Blues,
                                  normalize=normalize)
@@ -35,6 +38,6 @@ for title, normalize in titles_options:
     print(title)
     print(disp.confusion_matrix)
 
-plt.show()
+plt.show() """
 
 
