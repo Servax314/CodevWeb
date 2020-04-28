45d6e74e7834298970db1cd6b13da5aa1d38e9f4
HugoBcz <hugo.bouchez@orange.fr>
2020-04-28 22:03:00 +0200

    Add call function

--- ImageRecognition/IAM prediction/model.py
+++ ImageRecognition/IAM prediction/model.py
@@ -29,13 +29,13 @@ class CustomModel(tf.keras.Model):
 		self.pooling2 = tf.keras.layers.MaxPooling2D(poolsize[1],strides=None,padding='valid',data_format=channel_lasts)
 		
 		self.convolution3 = tf.keras.layers.Conv2D(features_values[2],Filter_size[2], activation = 'relu', name = 'convolution3')
-		self.pooling2 = tf.keras.layers.MaxPooling2D(poolsize[2],strides=None,padding='valid',data_format=channel_lasts)
+		self.pooling3 = tf.keras.layers.MaxPooling2D(poolsize[2],strides=None,padding='valid',data_format=channel_lasts)
 		
 		self.convolution4 = tf.keras.layers.Conv2D(features_values[3],Filter_size[3], activation = 'relu', name = 'convolution4')
-		self.pooling2 = tf.keras.layers.MaxPooling2D(poolsize[3],strides=None,padding='valid',data_format=channel_lasts)
+		self.pooling4 = tf.keras.layers.MaxPooling2D(poolsize[3],strides=None,padding='valid',data_format=channel_lasts)
 		
 		self.convolution5 = tf.keras.layers.Conv2D(features_values[4],Filter_size[4], activation = 'relu', name = 'convolution5')
-		self.pooling2 = tf.keras.layers.MaxPooling2D(poolsize[4],strides=None,padding='valid',data_format=channel_lasts)
+		self.pooling5 = tf.keras.layers.MaxPooling2D(poolsize[4],strides=None,padding='valid',data_format=channel_lasts)
 		
 		#Insert LSTM Layers (RNN Layers)
 
@@ -47,4 +47,24 @@ class CustomModel(tf.keras.Model):
 		#Build Bidirectional layer
 		self.bidirectional_layer = tf.keras.layers.Bidirectional(rnn_layer1,rnn_layer2,)
 
-	def call(self,_)
+	def call(self,inputs):
+		
+		#execute your operations
+		op1 = self.convolution1(inputs)
+		op1_pool = self.pooling1(op1)
+
+		op2 = self.convolution2(op1_pool)
+		op2_pool = self.pooling2(op2)
+
+		op3 = self.convolution3(op2_pool)
+		op3_pool = self.pooling3(op3)
+
+		op4 = self.convolution4(op3_pool)
+		op4_pool = self.pooling4(op4)
+
+		op5 = self.convolution5(op4_pool)
+		op5_pool = self.pooling5(op5)
+
+		output = self.bidirectionnal_layer(op5_pool)
+
+		return output
