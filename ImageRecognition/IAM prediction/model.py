54e7e534de4d275d5a908a0661b96174ef24c5bc
HugoBcz <hugo.bouchez@orange.fr>
2020-04-28 22:32:07 +0200

    Change parameters of the bidirectional layers

--- ImageRecognition/IAM prediction/model.py
+++ ImageRecognition/IAM prediction/model.py
@@ -23,29 +23,29 @@ class CustomModel(tf.keras.Model):
 		#By default, strides values are fixed to 1
 
 		self.convolution1 = tf.keras.layers.Conv2D(features_values[0],Filter_size[0], activation = 'relu', name = 'convolution1')
-		self.pooling1 = tf.keras.layers.MaxPooling2D(poolsize[0],strides=None,padding='valid',data_format=channel_lasts)
+		self.pooling1 = tf.keras.layers.MaxPooling2D(poolsize[0],strides=None,padding='valid',data_format=None)
 		
 		self.convolution2 = tf.keras.layers.Conv2D(features_values[1],Filter_size[1], activation = 'relu', name = 'convolution2')
-		self.pooling2 = tf.keras.layers.MaxPooling2D(poolsize[1],strides=None,padding='valid',data_format=channel_lasts)
+		self.pooling2 = tf.keras.layers.MaxPooling2D(poolsize[1],strides=None,padding='valid',data_format=None)
 		
 		self.convolution3 = tf.keras.layers.Conv2D(features_values[2],Filter_size[2], activation = 'relu', name = 'convolution3')
-		self.pooling3 = tf.keras.layers.MaxPooling2D(poolsize[2],strides=None,padding='valid',data_format=channel_lasts)
+		self.pooling3 = tf.keras.layers.MaxPooling2D(poolsize[2],strides=None,padding='valid',data_format=None)
 		
 		self.convolution4 = tf.keras.layers.Conv2D(features_values[3],Filter_size[3], activation = 'relu', name = 'convolution4')
-		self.pooling4 = tf.keras.layers.MaxPooling2D(poolsize[3],strides=None,padding='valid',data_format=channel_lasts)
+		self.pooling4 = tf.keras.layers.MaxPooling2D(poolsize[3],strides=None,padding='valid',data_format=None)
 		
 		self.convolution5 = tf.keras.layers.Conv2D(features_values[4],Filter_size[4], activation = 'relu', name = 'convolution5')
-		self.pooling5 = tf.keras.layers.MaxPooling2D(poolsize[4],strides=None,padding='valid',data_format=channel_lasts)
+		self.pooling5 = tf.keras.layers.MaxPooling2D(poolsize[4],strides=None,padding='valid',data_format=None)
 		
 		#Insert LSTM Layers (RNN Layers)
 
 		lstm_numbers=[256,256]
 
-		rnn_layer1 = tf.keras.layers.LSTM(lstm_numbers[0],return_sequences = True, stateful = True, name = 'rnn_layer1')
-		rnn_layer2 = tf.keras.layers.LSTM(lstm_numbers[1],return_sequences = True, stateful = True, name = 'rnn_layer2')
+		rnn_layer1 = tf.keras.layers.LSTM(lstm_numbers[0],return_sequences = True, stateful = True,name = 'rnn_layer1')
+		rnn_layer2 = tf.keras.layers.LSTM(lstm_numbers[1],return_sequences = True, stateful = True, go_backwards = True, name = 'rnn_layer2')
 		
 		#Build Bidirectional layer
-		self.bidirectional_layer = tf.keras.layers.Bidirectional(rnn_layer1,rnn_layer2,)
+		self.bidirectional_layer = tf.keras.layers.Bidirectional(rnn_layer1, backward_layer = rnn_layer2)
 
 	def call(self,inputs):
 		
@@ -68,3 +68,5 @@ class CustomModel(tf.keras.Model):
 		output = self.bidirectionnal_layer(op5_pool)
 
 		return output
+
+model = CustomModel()
