2d08cd21564fbaaba135accab6a6d113caf78562
HugoBcz <hugo.bouchez@orange.fr>
2020-05-24 11:18:17 +0200

    1

--- Connect.py
+++ Connect.py
@@ -1,22 +1,26 @@
 import pymongo
 from pymongo import MongoClient
+import gridfs
+import numpy as np
+import matplotlib.pyplot as plt
 
-client = MongoClient("mongodb+srv://nicolasservot:<password>@clustercodev-vuw7l.mongodb.net/test?retryWrites=true&w=majority")
-db = client.get_database('test')
-uploads_chunks = db.uploads_chunks
-download = db.download
+client = MongoClient("mongodb+srv://nicolasservot:nicolasservot@clustercodev-vuw7l.mongodb.net/test?retryWrites=true&w=majority")
+db = client['test']
+
+upload = gridfs.GridFS(db,collection='uploads')
+l = upload.list()
+print(l)
+upload.find()
+fileName = l[1]
+file = upload.find_one({"filename":fileName})
+img = file.read()
+
+print(img)
+#plt.imshow(img)
+#plt.show()
 
 #Count the number of document in uploads_chunks
-number_docs = uploads_chunks.count_documents({})
+#number_docs = upload.count_documents({})
+#print(number_docs)
 
-
-#Create new document
-post = {
-	'body': ''
-	'percentage": ''
-	'user':  ''
-}
-
-
-download.insert_one(post)
 
