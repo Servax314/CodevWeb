971c8d993fd7d43bd177afe9429decf0d872c982
HugoBcz <hugo.bouchez@orange.fr>
2020-05-25 11:48:31 +0200

    Add changes

--- ImageRecognition/IAM prediction/main.py
+++ ImageRecognition/IAM prediction/main.py
@@ -96,3 +96,50 @@ def infer(model, fnImg):
 	print('Recognized:', '"' + recognized[0] + '"')
 	print('Probability:', probability[0])
 
+def main():
+	"main function"
+	# optional command line args
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--train', help='train the NN', action='store_true')
+	parser.add_argument('--validate', help='validate the NN', action='store_true')
+	parser.add_argument('--beamsearch', help='use beam search instead of best path decoding', action='store_true')
+	parser.add_argument('--wordbeamsearch', help='use word beam search instead of best path decoding', action='store_true')
+	parser.add_argument('--dump', help='dump output of NN to CSV file(s)', action='store_true')
+
+	args = parser.parse_args()
+
+	decoderType = DecoderType.BestPath
+	if args.beamsearch:
+		decoderType = DecoderType.BeamSearch
+	elif args.wordbeamsearch:
+		decoderType = DecoderType.WordBeamSearch
+
+	# train or validate on IAM dataset	
+	if args.train or args.validate:
+		# load training data, create TF model
+		loader = DataLoader(FilePaths.fnTrain, Model.batchSize, Model.imgSize, Model.maxTextLen)
+
+		# save characters of model for inference mode
+		open(FilePaths.fnCharList, 'w').write(str().join(loader.charList))
+		
+		# save words contained in dataset into file
+		open(FilePaths.fnCorpus, 'w').write(str(' ').join(loader.trainWords + loader.validationWords))
+
+		# execute training or validation
+		if args.train:
+			model = Model(loader.charList, decoderType)
+			train(model, loader)
+		elif args.validate:
+			model = Model(loader.charList, decoderType, mustRestore=True)
+			validate(model, loader)
+
+	# infer text on test image
+	else:
+		print(open(FilePaths.fnAccuracy).read())
+		model = Model(open(FilePaths.fnCharList).read(), decoderType, mustRestore=True, dump=args.dump)
+		infer(model, FilePaths.fnInfer)
+
+
+if __name__ == '__main__':
+	main()
+
