b8ffc476a37299d50112ef2d4038915df1362121
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-05-28 10:38:52 +0200

    praterie

--- routes/documents.js
+++ routes/documents.js
@@ -20,7 +20,7 @@ router.get('/image/:filename', function(req,res) {
 
 //download numerical document
 router.get('/prediction', function(req,res){
-  var dataToSend= 'moula';
+  var dataToSend= 'Hello';
   const python = spawn('python3', ['../SimpleHTR/src/main.py', '../SimpleHTR/src/test1.jpg']);
   python.stdout.on('data', function (data) {
     console.log('Pipe data from python script ...');
