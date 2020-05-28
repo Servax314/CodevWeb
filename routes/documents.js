2d2b7ba0f348e5772994aefbb289d66f4116558c
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-05-28 10:30:59 +0200

    gogo

--- routes/documents.js
+++ routes/documents.js
@@ -20,8 +20,12 @@ router.get('/image/:filename', function(req,res) {
 
 //download numerical document
 router.get('/prediction', function(req,res){
-  var dataToSend;
+  var dataToSend= 'moula';
   const python = spawn('python3', ['-W ignore ','../SimpleHTR/src/main.py', '-p', '../SimpleHTR/src/test1.jpg']);
+  python.stdout.on('data', function (data) {
+    console.log('Pipe data from python script ...');
+    dataToSend = data.toString();
+  });
   python.on('close', (code) => {
     console.log(`child process closed`);
     res.json(dataToSend)
