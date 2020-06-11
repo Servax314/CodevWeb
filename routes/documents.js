3f0fcebbbf185bec45b2b147cc0f0871a23d9c9a
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-06-11 11:03:13 +0200

    for u hugo

--- routes/documents.js
+++ routes/documents.js
@@ -3,12 +3,13 @@ const router = express.Router();
 var mongoose = require("mongoose");
 const {spawn} = require('child_process');
 const path = require('path');
+var fs = require('fs');
 
 const upload = require('../config/storageGF.js');
 
 //upload handwritten document to db
 router.post('/upload', upload.single('file'), function(req,res){
-  res.redirect('/prediction')
+  res.redirect('/prediction/80723457892345789234578901345')
 });
 
 router.get('/image/:filename', function(req,res) {
@@ -22,13 +23,21 @@ router.get('/image/:filename', function(req,res) {
 //download numerical document
 router.get('/prediction/:id', function(req,res){
   var dataToSend;
-  const python = spawn('python3', ['-W ignore', '/SimpleHTR/src/main.py -p', 'test1.png']);
-  python.stdout.on('data', function (data) {
+  const python = spawn('python3', ['/Users/NicolasServot/Desktop/Hackathon/CodevWeb/SimpleHTR/src/main.py', '/Users/NicolasServot/Desktop/Hackathon/CodevWeb/SimpleHTR/src/test1.png']);
+  const python2 = spawn('python3', ['/home/hugo/Documents/ML/Project/CodevWeb/SimpleHTR/src/main.py']);
+  python2.stdout.on('data', function (data) {
     console.log('Pipe data from python script ...');
     dataToSend = data.toString();
   });
-  python.on('close', (code) => {
+  python2.on('close', (code) => {
     console.log(`child process closed`);
+    fs.appendFile('log.txt', dataToSend, function (err) {
+  if (err) {
+    next();
+  } else {
+    // done
+  }
+})
     res.send(dataToSend)
     //console.log(res)
   });
