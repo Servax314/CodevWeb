35d6523c96da264c32cafd4146d9f7745d0b7bc7
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-06-24 23:14:53 +0200

    xx

--- routes/documents.js
+++ routes/documents.js
@@ -3,7 +3,7 @@ const router = express.Router();
 var mongoose = require("mongoose");
 const {spawn} = require('child_process');
 const path = require('path');
-var fs = require('fs');
+const fs = require('fs');
 const multer = require("multer");
 const {checkAuthenticated, checkAdmin} = require('../config/auth.js');
 const upload = require('../config/storageGF.js');
@@ -23,35 +23,19 @@ const vision = require('@google-cloud/vision');
 
 //upload handwritten document to db
 router.post('/upload',checkAuthenticated,  uploadLocal.single('file'), function(req,res){
-  console.log(req.file.filename)
-  filePath = path.resolve('fileUpload/' + req.file.filename)
+  const fileName = req.file.filename
+  filePath = path.resolve('fileUpload/' + fileName)
   const clientOptions = {apiEndpoint: 'eu-vision.googleapis.com'};
   const client = new vision.ImageAnnotatorClient();
   const result = client.documentTextDetection(filePath)
     .then(result => {
       const fullTextAnnotation = result[0].fullTextAnnotation;
       console.log(`Full text: ${fullTextAnnotation.text}`);
-      fullTextAnnotation.pages.forEach(page => {
-        page.blocks.forEach(block => {
-          console.log(`Block confidence: ${block.confidence}`);
-          block.paragraphs.forEach(paragraph => {
-            console.log(`Paragraph confidence: ${paragraph.confidence}`);
-            paragraph.words.forEach(word => {
-              const wordText = word.symbols.map(s => s.text).join('');
-              console.log(`Word text: ${wordText}`);
-              console.log(`Word confidence: ${word.confidence}`);
-              word.symbols.forEach(symbol => {
-                console.log(`Symbol text: ${symbol.text}`);
-                console.log(`Symbol confidence: ${symbol.confidence}`);
-        });
-      });
-    });
-  });
-});
     });
 
-
 });
+
+
 
 router.get('/image/:filename', checkAdmin, function(req,res) {
   const gfs = require('../server.js');
