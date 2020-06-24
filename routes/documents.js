1036a2cd9828467c6ccbfcbbd7ef0a407e4e0ed4
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-06-24 21:56:55 +0200

    localstorage

--- routes/documents.js
+++ routes/documents.js
@@ -4,24 +4,50 @@ var mongoose = require("mongoose");
 const {spawn} = require('child_process');
 const path = require('path');
 var fs = require('fs');
+const multer = require("multer");
 const {checkAuthenticated, checkAdmin} = require('../config/auth.js');
 const upload = require('../config/storageGF.js');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve('abc/'))
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now()+"." + file.originalname.split('.')[file.originalname.split('.').length -1])
+  }
+})
+
+const uploadLocal = multer({ storage: storage })
+
 
 const vision = require('@google-cloud/vision');
 
 //upload handwritten document to db
-router.post('/upload',checkAuthenticated, upload.single('file'), function(req,res){
-  
-  console.log(req.file)
+router.post('/upload',checkAuthenticated,  uploadLocal.single('file'), function(req,res){
+  console.log(req.file.filename)
+  filePath = path.resolve('abc/' + req.file.filename)
   const clientOptions = {apiEndpoint: 'eu-vision.googleapis.com'};
   const client = new vision.ImageAnnotatorClient();
-  const result = client.documentTextDetection(
-    'https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/126259992/original/d377377b6758a398b9ab6cbe1d27fd536acfcca8/convert-any-handwritten-english-or-hindi-text-to-word-or-excel.jpg'
-  )
+  const result = client.documentTextDetection(filePath)
     .then(result => {
-      console.log(result)
       const fullTextAnnotation = result[0].fullTextAnnotation;
-      console.log(fullTextAnnotation.text);
+      console.log(`Full text: ${fullTextAnnotation.text}`);
+      fullTextAnnotation.pages.forEach(page => {
+        page.blocks.forEach(block => {
+          console.log(`Block confidence: ${block.confidence}`);
+          block.paragraphs.forEach(paragraph => {
+            console.log(`Paragraph confidence: ${paragraph.confidence}`);
+            paragraph.words.forEach(word => {
+              const wordText = word.symbols.map(s => s.text).join('');
+              console.log(`Word text: ${wordText}`);
+              console.log(`Word confidence: ${word.confidence}`);
+              word.symbols.forEach(symbol => {
+                console.log(`Symbol text: ${symbol.text}`);
+                console.log(`Symbol confidence: ${symbol.confidence}`);
+        });
+      });
+    });
+  });
+});
     });
 
 
