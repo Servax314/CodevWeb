7363e188835ae9b94037284963efd3cd5ae72c70
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-06-25 01:26:36 +0200

    logout

--- public/accueil/js/accueil.js
+++ public/accueil/js/accueil.js
@@ -1,25 +1,15 @@
-/*download an image
-$("<a>")
-    .attr("href", "img.png")
-    .attr("download", "output.png")
-    .appendTo("body")
-    .click()
-    .remove();*/
 
-/*var a = document.createElement('a');
-a.href = "img.png";
-a.download = "output.png";
-document.body.appendChild(a);
-a.click();
-document.body.removeChild(a);
-
-var fileSelect = document.getElementById("fileSelect"),
-  fileElem = document.getElementById("fileElem");
-
-fileSelect.addEventListener("click", function (e) {
-  if (fileElem) {
-    fileElem.click();
-  }
-  e.preventDefault(); // empêche la navigation vers "#"
-}, false);*/
-
+function funcDisconnect() {
+  url = '/logout';
+  let headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  let fetchData = {
+        method: 'GET',
+        body: JSON.stringify(data),
+        headers: headers
+      };
+  fetch(url,fetchData)
+    .then(function(res){
+      window.location.replace(res.url);
+    }
+}
--- routes/accueil.js
+++ routes/accueil.js
@@ -5,4 +5,9 @@ const {checkAuthenticated} = require('../config/auth.js');
 //accueil page
 router.get('/', checkAuthenticated, (req,res) => res.sendFile('accueil.html', { root: './views'}));
 
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/login');
+});
+
 module.exports = router;
